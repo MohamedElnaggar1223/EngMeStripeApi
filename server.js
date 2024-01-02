81638f9b8b9d5f26ca4a34594a72b4e2ffb5216f
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-01-02 14:08:29 +0200

    Stripe

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.post('/create-checkout-session', async (req, res) => {
             currency: "egp",
             product_data: {
                 name: program.name,
-                images: [program.image.length < 2048 ? program.image : 'https://firebasestorage.googleapis.com/v0/b/engmedemo.appspot.com/o/ProgramImages%2Fcardpic-min.png?alt=media&token=6e306470-2c2f-46fb-be2f-bb8a948741e2']
+                images: [program.image.length < 2048 && program.image.length > 0 ? program.image : 'https://firebasestorage.googleapis.com/v0/b/engmedemo.appspot.com/o/ProgramImages%2Fcardpic-min.png?alt=media&token=6e306470-2c2f-46fb-be2f-bb8a948741e2']
             },
             unit_amount: program.price * 100
         },
