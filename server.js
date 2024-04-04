28ad3ac3e2a84fa94a3fb72bb8a74d2a5ecbfb2d
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-04-04 14:21:55 +0200

    Discount

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ app.post('/create-checkout-session', async (req, res) => {
                     name: program.name,
                     images: [program.image.length < 2048 && program.image.length > 0 ? program.image : 'https://firebasestorage.googleapis.com/v0/b/engmedemo.appspot.com/o/ProgramImages%2Fcardpic-min.png?alt=media&token=6e306470-2c2f-46fb-be2f-bb8a948741e2']
                 },
-                unit_amount: (program.price * (1 - (program?.discount ?? 0))) * 100
+                unit_amount: (program.price * (1 - ((program?.discount ?? 0) / 100))) * 100
             },
             quantity: 1
         }
@@ -198,7 +198,7 @@ app.post('/create-checkout-session', async (req, res) => {
             payment_intent_data: {
                 transfer_data: {
                     destination: accountId,
-                    amount: Math.floor(((program.price * (1 - (program?.discount ?? 0))) * ( Number(program.teacherShare) / 100 )) * 100),
+                    amount: Math.floor(((program.price * (1 - ((program?.discount ?? 0) / 100))) * ( Number(program.teacherShare) / 100 )) * 100),
                 },
             },
             metadata: {
